05ce572d6268cd79621516e2186f509180460e61
m4nuul <nikitakortrlr020@gmail.com>
2025-04-15 12:14:01 +0700

    created and edit 2_py

---
+++ 2_py.py
@@ -0,0 +1,10 @@
+string = input("Введите строку: ")
+array_of_char = set()
+for i in range(len(string)):
+   if string[i].isalpha and string[i] not in array_of_char:
+      array_of_char.add(string[i])
+if len(array_of_char) == 26:
+   print("True")
+else:
+   print("False")
+
