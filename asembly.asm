4688de10ac0106d5d3a2db45cb3b45f68a17c7a2
m4nuul <nikitakortrlr020@gmail.com>
2025-04-23 11:12:37 +0700

    rewrite assembly code

--- asembly.asm
+++ asembly.asm
@@ -1,50 +1,42 @@
+section .data
+    elements dd 3, 4, 3, 5, 5, 6, 6       ; Массив чисел
+    elements_count equ ($ - elements) / 4
+
 section .bss
-    count_of_repits resd 1      ; Переменная для хранения количества повторений
-    last_element resd 1         ; Переменная для хранения последнего введенного числа
-    element resd 1              ; Переменная для хранения текущего числа
+    count_of_repits resd 1          ; Счётчик
+    last_element resd 1             ; Предыдущий элемент
 
 section .text
     global _start
 
 _start:
-    ; Инициализация переменных
-    mov dword [count_of_repits], 0  ; Счётчик повторов = 0
-    mov dword [last_element], -1    ; last_element инициализируем значением -1 (ничем не совпадает)
-    
-    ; Количество чисел задаём напрямую (например, 5)
-    mov eax, 5                     ; range = 5
-    mov ecx, eax                   ; Сохраняем range в ecx (цикл будет выполняться range раз)
+    ; Инициализация
+    mov dword [count_of_repits], 0
+    mov dword [last_element], -1
+
+    ; Настройка цикла
+    mov ecx, elements_count         ; Счётчик
+    mov rsi, elements               ; Указатель 
 
 input_loop:
-    ; Эмуляция ввода элемента (например, 3, 3, 4, 5, 5)
-    ; Для упрощения примера, числовые значения задаются вручную
-    mov eax, 3                     ; Элемент = 3
-    ; mov eax, 4                     ; Элемент = 4
-    ; mov eax, 5                     ; Элемент = 5
-    ; mov eax, 5                     ; Элемент = 5
-    mov [element], eax
+    ; Загрузка элемента
+    mov eax, [rsi]
+    add rsi, 4                      ; Следующий элемент
 
-    ; Сравниваем текущий элемент с last_element
-    mov eax, [element]
-    mov ebx, [last_element]
-    cmp eax, ebx
-    jne no_repeat                  ; Если элемент не равен последнему, пропускаем
+    ; Сравнение с предыдущим
+    cmp eax, [last_element]
+    jne no_repeat
 
-    ; Если элементы одинаковы, увеличиваем счётчик повторов
-    mov eax, [count_of_repits]
-    inc eax
-    mov [count_of_repits], eax
+    ; Увеличиваем счётчик
+    inc dword [count_of_repits]
 
 no_repeat:
-    ; Обновляем last_element
-    mov eax, [element]
+    ; Обновляем предыдущий элемент
     mov [last_element], eax
 
-    ; Переходим к следующему элементу (уменьшаем счетчик)
     loop input_loop
 
-    ; Завершаем программу, возвращая количество повторов в качестве exit code
-    mov eax, [count_of_repits]     ; Получаем количество повторений
-    mov ebx, eax                   ; Помещаем его в ebx (exit code)
-    mov eax, 1                     ; Системный вызов exit
-    int 0x80                       ; Вызов ядра
+    ; Завершение программы
+    mov rax, 60                     ; sys_exit
+    mov edi, [count_of_repits]      ; exit code
+    syscall
