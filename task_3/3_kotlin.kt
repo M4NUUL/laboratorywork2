6fbd91399e75bd2be7608ea3056feedcd3dbcd0f
m4nuul <nikitakortrlr020@gmail.com>
2025-04-25 14:25:18 +0700

    rewrite kotlin

--- task_3/3_kotlin.kt
+++ task_3/3_kotlin.kt
@@ -1,34 +1,19 @@
+import java.util.Scanner
+
 fun main() {
+    val scanner = Scanner(System.`in`)
     print("Введите количество чисел: ")
-    val repets = readLine()?.toIntOrNull() ?: return
+    val range = scanner.nextInt()
+    var countOfRepits = 0
     var lastElement = -1
-    var repit = 0
 
-    repeat(repets) {
-        val element = readLine()?.toIntOrNull() ?: 0
+    repeat(range) {
+        val element = scanner.nextInt()
         if (element == lastElement) {
-            repit++
-        }
-        lastElemefun main() {
-    print("Enter the number of inputs: ")
-    val totalNumbers = readLine()?.toIntOrNull()
-
-    var lastElement: Int? = null
-    var repeatCount = 0
-
-    repeat(totalNumbers) {
-        print("Enter a number: ")
-        val element = readLine()?.toIntOrNull()
-
-        if (element == lastElement) {
-            repeatCount++
+            countOfRepits++
         }
         lastElement = element
     }
 
-    println("Number of repetitions: $repeatCount")
-}nt = element
-    }
-
-    println("Количество повторов: $repit")
+    println("Количество повторов: $countOfRepits")
 }
